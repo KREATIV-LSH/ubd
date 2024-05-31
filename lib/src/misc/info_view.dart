991e26bd70f0d7bd32ef130df62f0f7c5651480d
Luis Hutterli <lshutterli@gmail.com>
2024-05-31 09:04:59 +0200

    Updated info

--- lib/src/misc/info_view.dart
+++ lib/src/misc/info_view.dart
@@ -18,7 +18,7 @@ class InfoView extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('Das Zerfallsgesetz:', style: TextStyle(fontSize: textSize + 5, fontWeight: FontWeight.bold)),
+            const Text('Einfaches Zerfallsgesetz:', style: TextStyle(fontSize: textSize + 5, fontWeight: FontWeight.bold)),
             const Text("Defenitionen:", style: TextStyle(fontSize: textSize)),
             Math.tex(r'T_{1/2}: \text{Halbwertszeit von } ^{238}U', mathStyle: MathStyle.textCramped, textStyle: const TextStyle(fontSize: mathSize)),
             Math.tex(r'N_u(0): \text{Anzahl } ^{238}U\text{ zum Zeitpunkt 0}', mathStyle: MathStyle.textCramped, textStyle: const TextStyle(fontSize: mathSize)),
@@ -28,6 +28,16 @@ class InfoView extends StatelessWidget {
             const Text('Umformung nach t:', style: TextStyle(fontSize: textSize)),
             Math.tex(r't = \frac{-T_{1/2}\cdot log_{10}(X)}{log_{10}(2)}', mathStyle: MathStyle.textCramped, textStyle: const TextStyle(fontSize: mathSize)),
             Math.tex(r't = -T_{1/2}\cdot log_2(X)', mathStyle: MathStyle.textCramped, textStyle: const TextStyle(fontSize: mathSize)),
+            const Text('Erweitertes Zerfallsgesetz:', style: TextStyle(fontSize: textSize + 5, fontWeight: FontWeight.bold)),
+            const Text("Defenitionen:", style: TextStyle(fontSize: textSize)),
+            Math.tex(r"^{206}\mathrm{Pb}: \text{Bleiisotop } ^{206}\mathrm{Pb}, \text{ Zerfallsprodukt}", mathStyle: MathStyle.textCramped, textStyle: const TextStyle(fontSize: mathSize)),
+            Math.tex(r"^{238}\mathrm U: \text{Uranisotop } ^{238}\mathrm U, \text{ Ursprungsisotop}", mathStyle: MathStyle.textCramped, textStyle: const TextStyle(fontSize: mathSize)),
+            Math.tex(r"\lambda_{238}: \text{Zerfallskonstante von } ^{238}\mathrm U", mathStyle: MathStyle.textCramped, textStyle: const TextStyle(fontSize: mathSize)),
+            Math.tex(r"\lambda_{238} = \frac{\ln 2}{T_{1/2}} \approx 1.55125 \cdot 10^{-10} / Jahr", mathStyle: MathStyle.textCramped, textStyle: const TextStyle(fontSize: mathSize)),
+            const Text("Formel:", style: TextStyle(fontSize: textSize)),
+            Math.tex(r"^{206}\mathrm{Pb} = ^{238}\mathrm U \cdot (1-e^{-\lambda_{238} t})", mathStyle: MathStyle.textCramped, textStyle: const TextStyle(fontSize: mathSize)),
+            const Text("Umformung nach t:", style: TextStyle(fontSize: textSize)),
+            Math.tex(r"t = \frac{1}{\lambda_{238}} \cdot \ln \left( 1+ \frac{^{206}\mathrm{Pb}}{^{238}\mathrm U} \right)", mathStyle: MathStyle.textCramped, textStyle: const TextStyle(fontSize: mathSize))
           ],
         ),
       ),
