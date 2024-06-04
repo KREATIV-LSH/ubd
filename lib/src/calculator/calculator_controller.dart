bcedd794f037c17d75f80398a811993da766d51b
Luis Hutterli <lshutterli@gmail.com>
2024-06-04 22:04:12 +0200

    Responsive design on the info page

--- lib/src/calculator/calculator_controller.dart
+++ lib/src/calculator/calculator_controller.dart
@@ -131,8 +131,16 @@ class CalculatorController {
     num uranium = num.tryParse(t1!)!;
     num lead = num.tryParse(t2!)!;
 
-    if (uranium == 0 || lead == 0) {
+    if (uranium == 0 && lead == 0) {
       return (true, AppLocalizations.of(context)!.resultError, 0);
+    }
+
+    if(uranium == 0) {
+      return (true, AppLocalizations.of(context)!.resultInfinity, 0);
+    }
+
+    if(lead == 0) {
+      return (false, AppLocalizations.of(context)!.resultZero, 0);
     }
 
     num t = 1 / lambda238 * log(1 + (lead / uranium)) * 1e11;
@@ -148,8 +156,16 @@ class CalculatorController {
     num uranium = num.tryParse(t1!)!;
     num lead = num.tryParse(t2!)!;
 
-    if (uranium == 0 || lead == 0) {
+    if (uranium == 0 && lead == 0) {
       return (true, AppLocalizations.of(context)!.resultError, 0);
+    }
+
+    if(uranium == 0) {
+      return (true, AppLocalizations.of(context)!.resultInfinity, 0);
+    }
+
+    if(lead == 0) {
+      return (false, AppLocalizations.of(context)!.resultZero, 0);
     }
 
     num t = 1 / lambda235 * log(1 + (lead / uranium)) * 1e10;
--- lib/src/misc/info_view.dart
+++ lib/src/misc/info_view.dart
@@ -9,114 +9,146 @@ class InfoView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Info"),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.description, size: 35),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: const Text("Legal / Impressum"),
-                      content: const SingleChildScrollView(
-                        child: Text(
-                            "© 2024 - Alle Rechte vorbehalten.\n\n"
-                            "Verantwortlicher / Kontakt:\n"
-                            "Luis Hutterli\n"
-                            "Bärenstrasse 24, 8280 Kreuzlingen, Schweiz\n"
-                            "079 874 04 48 - luis.hutterli@stud.kftg.ch\n\n"
-                            "Urheberrechtshinweis:\n"
-                            "Alle Inhalte und Werke auf dieser Website, einschließlich Quellcode, sind das Eigentum von Luis Hutterli und dürfen ohne ausdrückliche schriftliche Genehmigung nicht kopiert, modifiziert, veröffentlicht, übertragen, verteilt oder auf andere Weise verwendet werden.\n\n"
-                            "Datenschutzhinweis:\n"
-                            "Durch die Nutzung dieses Dienstes erklären Sie sich damit einverstanden, dass Ihre Nutzungsdaten möglicherweise durch uns oder Dritte gespeichert und verarbeitet werden.\nDie Verarbeiteten Daten sind anonymisiert und lassen sich nicht auf Sie zurückführen.\nIhre Einstellungen und Präferenzen werden nur lokal auf Ihrem Gerät gespeichert.\nMögliche Dritte: Google, Cloudflare, Firebase\n"),
-                      ), // Add this
-                      actions: <Widget>[
-                        IconButton(
-                          icon: const Icon(Icons.close),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
+      appBar: AppBar(
+        title: const Text("Info"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.description, size: 35),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text("Legal / Impressum"),
+                    content: const SingleChildScrollView(
+                      child: Text("© 2024 - Alle Rechte vorbehalten.\n\n"
+                          "Verantwortlicher / Kontakt:\n"
+                          "Luis Hutterli\n"
+                          "Bärenstrasse 24, 8280 Kreuzlingen, Schweiz\n"
+                          "079 874 04 48 - luis.hutterli@stud.kftg.ch\n\n"
+                          "Urheberrechtshinweis:\n"
+                          "Alle Inhalte und Werke auf dieser Website, einschließlich Quellcode, sind das Eigentum von Luis Hutterli und dürfen ohne ausdrückliche schriftliche Genehmigung nicht kopiert, modifiziert, veröffentlicht, übertragen, verteilt oder auf andere Weise verwendet werden.\n\n"
+                          "Datenschutzhinweis:\n"
+                          "Durch die Nutzung dieses Dienstes erklären Sie sich damit einverstanden, dass Ihre Nutzungsdaten möglicherweise durch uns oder Dritte gespeichert und verarbeitet werden.\nDie Verarbeiteten Daten sind anonymisiert und lassen sich nicht auf Sie zurückführen.\nIhre Einstellungen und Präferenzen werden nur lokal auf Ihrem Gerät gespeichert.\nMögliche Dritte: Google, Cloudflare, Firebase\n"),
+                    ), // Add this
+                    actions: <Widget>[
+                      IconButton(
+                        icon: const Icon(Icons.close),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            tooltip: "Legal",
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              bool isWideScreen = constraints.maxWidth > 700;
+              return isWideScreen
+                  ? Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(child: contentColumn1()),
+                        const SizedBox(width: 16),
+                        Expanded(child: contentColumn2()),
+                      ],
+                    )
+                  : Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        contentColumn1(),
+                        const SizedBox(height: 16),
+                        contentColumn2(),
                       ],
                     );
-                  },
-                );
-              },
-              tooltip: "Legal",
-            ),
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("Einfaches Zerfallsgesetz:",
-                    style: TextStyle(
-                        fontSize: textSize + 5, fontWeight: FontWeight.bold)),
-                const Text("Definitionen:",
-                    style: TextStyle(fontSize: textSize)),
-                Math.tex(r"T_{1/2}: \text{Halbwertszeit von } ^{238}U",
-                    mathStyle: MathStyle.textCramped,
-                    textStyle: const TextStyle(fontSize: mathSize)),
-                Math.tex(
-                    r"N_u(0): \text{Anzahl } ^{238}U\text{ zum Zeitpunkt 0}",
-                    mathStyle: MathStyle.textCramped,
-                    textStyle: const TextStyle(fontSize: mathSize)),
-                Math.tex(
-                    r"N_u(t): \text{Anzahl } ^{238}U\text{ zum Zeitpunkt t}",
-                    mathStyle: MathStyle.textCramped,
-                    textStyle: const TextStyle(fontSize: mathSize)),
-                const Text("Formel:", style: TextStyle(fontSize: textSize)),
-                Math.tex(r"N_u(t)=N_u(0)\cdot 2^{\frac{-t}{T_{1/2}}}",
-                    mathStyle: MathStyle.textCramped,
-                    textStyle: const TextStyle(fontSize: mathSize)),
-                const Text("Umformung nach t:",
-                    style: TextStyle(fontSize: textSize)),
-                Math.tex(r"t = \frac{-T_{1/2}\cdot log_{10}(X)}{log_{10}(2)}",
-                    mathStyle: MathStyle.textCramped,
-                    textStyle: const TextStyle(fontSize: mathSize)),
-                Math.tex(r"t = -T_{1/2}\cdot log_2(X)",
-                    mathStyle: MathStyle.textCramped,
-                    textStyle: const TextStyle(fontSize: mathSize)),
-                const Text("Erweitertes Zerfallsgesetz:",
-                    style: TextStyle(
-                        fontSize: textSize + 5, fontWeight: FontWeight.bold)),
-                const Text("Definitionen:",
-                    style: TextStyle(fontSize: textSize)),
-                Math.tex(
-                    r"^{206}\mathrm{Pb}: \text{Bleiisotop } ^{206}\mathrm{Pb}, \text{ Zerfallsprodukt}",
-                    mathStyle: MathStyle.textCramped,
-                    textStyle: const TextStyle(fontSize: mathSize)),
-                Math.tex(
-                    r"^{238}\mathrm U: \text{Uranisotop } ^{238}\mathrm U, \text{ Ursprungsisotop}",
-                    mathStyle: MathStyle.textCramped,
-                    textStyle: const TextStyle(fontSize: mathSize)),
-                Math.tex(
-                    r"\lambda_{238}: \text{Zerfallskonstante von } ^{238}\mathrm U",
-                    mathStyle: MathStyle.textCramped,
-                    textStyle: const TextStyle(fontSize: mathSize)),
-                Math.tex(
-                    r"\lambda_{238} = \frac{\ln 2}{T_{1/2}} \approx 1.55125 \cdot 10^{-10} / Jahr",
-                    mathStyle: MathStyle.textCramped,
-                    textStyle: const TextStyle(fontSize: mathSize)),
-                const Text("Formel:", style: TextStyle(fontSize: textSize)),
-                Math.tex(
-                    r"^{206}\mathrm{Pb} = ^{238}\mathrm U \cdot (1-e^{-\lambda_{238} t})",
-                    mathStyle: MathStyle.textCramped,
-                    textStyle: const TextStyle(fontSize: mathSize)),
-                const Text("Umformung nach t:",
-                    style: TextStyle(fontSize: textSize)),
-                Math.tex(
-                    r"t = \frac{1}{\lambda_{238}} \cdot \ln \left( 1+ \frac{^{206}\mathrm{Pb}}{^{238}\mathrm U} \right)",
-                    mathStyle: MathStyle.textCramped,
-                    textStyle: const TextStyle(fontSize: mathSize))
-              ],
-            ),
+            },
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  Widget contentColumn1() {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("First Text Box",
+            style:
+                TextStyle(fontSize: textSize + 5, fontWeight: FontWeight.bold)),
+        Text("This is the content of the first text box.",
+            style: TextStyle(fontSize: textSize)),
+      ],
+    );
+  }
+
+  Widget contentColumn2() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text("Einfaches Zerfallsgesetz:",
+            style:
+                TextStyle(fontSize: textSize + 5, fontWeight: FontWeight.bold)),
+        const Text("Definitionen:", style: TextStyle(fontSize: textSize)),
+        Math.tex(r"T_{1/2}: \text{Halbwertszeit von } ^{238}U",
+            mathStyle: MathStyle.textCramped,
+            textStyle: const TextStyle(fontSize: mathSize)),
+        Math.tex(r"N_u(0): \text{Anzahl } ^{238}U\text{ zum Zeitpunkt 0}",
+            mathStyle: MathStyle.textCramped,
+            textStyle: const TextStyle(fontSize: mathSize)),
+        Math.tex(r"N_u(t): \text{Anzahl } ^{238}U\text{ zum Zeitpunkt t}",
+            mathStyle: MathStyle.textCramped,
+            textStyle: const TextStyle(fontSize: mathSize)),
+        const Text("Formel:", style: TextStyle(fontSize: textSize)),
+        Math.tex(r"N_u(t)=N_u(0)\cdot 2^{\frac{-t}{T_{1/2}}}",
+            mathStyle: MathStyle.textCramped,
+            textStyle: const TextStyle(fontSize: mathSize)),
+        const Text("Umformung nach t:", style: TextStyle(fontSize: textSize)),
+        Math.tex(r"t = \frac{-T_{1/2}\cdot log_{10}(X)}{log_{10}(2)}",
+            mathStyle: MathStyle.textCramped,
+            textStyle: const TextStyle(fontSize: mathSize)),
+        Math.tex(r"t = -T_{1/2}\cdot log_2(X)",
+            mathStyle: MathStyle.textCramped,
+            textStyle: const TextStyle(fontSize: mathSize)),
+        const Text("Erweitertes Zerfallsgesetz:",
+            style:
+                TextStyle(fontSize: textSize + 5, fontWeight: FontWeight.bold)),
+        const Text("Definitionen:", style: TextStyle(fontSize: textSize)),
+        Math.tex(
+            r"^{206}\mathrm{Pb}: \text{Bleiisotop } ^{206}\mathrm{Pb}, \text{ Zerfallsprodukt}",
+            mathStyle: MathStyle.textCramped,
+            textStyle: const TextStyle(fontSize: mathSize)),
+        Math.tex(
+            r"^{238}\mathrm U: \text{Uranisotop } ^{238}\mathrm U, \text{ Ursprungsisotop}",
+            mathStyle: MathStyle.textCramped,
+            textStyle: const TextStyle(fontSize: mathSize)),
+        Math.tex(
+            r"\lambda_{238}: \text{Zerfallskonstante von } ^{238}\mathrm U",
+            mathStyle: MathStyle.textCramped,
+            textStyle: const TextStyle(fontSize: mathSize)),
+        Math.tex(
+            r"\lambda_{238} = \frac{\ln 2}{T_{1/2}} \approx 1.55125 \cdot 10^{-10} / Jahr",
+            mathStyle: MathStyle.textCramped,
+            textStyle: const TextStyle(fontSize: mathSize)),
+        const Text("Formel:", style: TextStyle(fontSize: textSize)),
+        Math.tex(
+            r"^{206}\mathrm{Pb} = ^{238}\mathrm U \cdot (1-e^{-\lambda_{238} t})",
+            mathStyle: MathStyle.textCramped,
+            textStyle: const TextStyle(fontSize: mathSize)),
+        const Text("Umformung nach t:", style: TextStyle(fontSize: textSize)),
+        Math.tex(
+            r"t = \frac{1}{\lambda_{238}} \cdot \ln \left( 1+ \frac{^{206}\mathrm{Pb}}{^{238}\mathrm U} \right)",
+            mathStyle: MathStyle.textCramped,
+            textStyle: const TextStyle(fontSize: mathSize))
+      ],
+    );
   }
 }
